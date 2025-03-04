39aba5bf47c50809c586070f5d3880a63dcd00c0
Matthew J Pearson <seaofcode@gmail.com>
2025-03-04 12:47:31 -0600

    Player Movement Rotation
    Rotation Input

--- RocketBoost2025/Assets/Scripts/Movement.cs
+++ RocketBoost2025/Assets/Scripts/Movement.cs
@@ -4,7 +4,9 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
     [SerializeField] float thrustStrength;
+    [SerializeField] float rotationStrength;
 
     Rigidbody rb;
 
@@ -16,14 +18,39 @@ public class Movement : MonoBehaviour
     void OnEnable()
     {
         thrust.Enable();
+        rotation.Enable();
     }
 
 
     void FixedUpdate()
     {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
+    {
         if (thrust.IsPressed())
         {
             rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
         }
+    }
+
+    private void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        if (rotationInput < 0)
+        {
+            ApplyRotation(Vector3.forward);
+        }
+        else if (rotationInput > 0)
+        {
+            ApplyRotation(Vector3.back);
+        }
+    }
+
+    private void ApplyRotation(Vector3 vectorDir)
+    {
+        transform.Rotate(rotationStrength * Time.fixedDeltaTime * vectorDir);
     }
 }
