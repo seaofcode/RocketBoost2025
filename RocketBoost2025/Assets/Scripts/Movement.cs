8cc43a1343bd3613145659af05848777b5253fbf
Matthew J Pearson <seaofcode@gmail.com>
2025-03-04 12:00:36 -0600

    Input Actions
    created Sandbox Scene, deleted sample scene, setting up for Input Action, Parented rocket prefab to empty GO, bind input keys to space and gamepad trigger. added rigidbody and box collider to Player Rocket, added Relative Force Vector3 up

---
+++ RocketBoost2025/Assets/Scripts/Movement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] InputAction thrust;
+    [SerializeField] float thrustStrength;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnEnable()
+    {
+        thrust.Enable();
+    }
+
+
+    void FixedUpdate()
+    {
+        if (thrust.IsPressed())
+        {
+            rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
+        }
+    }
+}
